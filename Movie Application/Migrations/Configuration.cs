90ab06b795592b939771054f1c3a5cb35eecc671
Sunku Saarthak <sunkusaarthak@gmail.com>
2024-03-23 13:41:01 +0530

    Database Code-First Migration Success

---
+++ Movie Application/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace Movie_Application.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Movie_Application.Models.MyDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Movie_Application.Models.MyDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
