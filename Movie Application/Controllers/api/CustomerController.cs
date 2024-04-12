57d7b34f8833e594e26cc3d8a411e0fd09aa2bc8
Sunku Saarthak <sunkusaarthak@gmail.com>
2024-04-12 23:18:37 +0530

    added api customer controller

---
+++ Movie Application/Controllers/api/CustomerController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Movie_Application.Controllers.api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+
+    }
+}
