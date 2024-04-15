532a7f5286683d010c4b6b54573698967e98ddad
Sunku Saarthak <sunkusaarthak@gmail.com>
2024-04-15 23:30:25 +0530

    added save

--- Movie Application/Controllers/api/MovieController.cs
+++ Movie Application/Controllers/api/MovieController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Movie_Application.Models;
+using Movie_Application.ViewModel;
 
 namespace Movie_Application.Controllers.api
 {
@@ -22,6 +23,37 @@ namespace Movie_Application.Controllers.api
         public Movie GetMovieById(int id)
         {
             return _context.Movies.SingleOrDefault(x => x.Id == id);
+        }
+
+        public IActionResult Save(Movie movie)
+        {
+            if (!ModelState.IsValid)
+            {
+                var movieFormViewModel = new MovieFormViewModel
+                {
+                    Movie = movie,
+                    GenreList = _context.Genres.ToList()
+                };
+                //return View("MovieForm", movieFormViewModel);
+            }
+            if (movie.Id == 0)
+            {
+                movie.DateInserted = DateTime.Now;
+                _context.Movies.Add(movie);
+            }
+            else
+            {
+                var movieInDb = _context.Movies.FirstOrDefault(m => m.Id == movie.Id);
+                if (movieInDb != null)
+                {
+                    movieInDb.Name = movie.Name;
+                    movieInDb.GenreId = movie.GenreId;
+                    movieInDb.Stock = movie.Stock;
+                    movieInDb.ReleaseDate = movie.ReleaseDate;
+                }
+            }
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Movies");
         }
     }
 }
