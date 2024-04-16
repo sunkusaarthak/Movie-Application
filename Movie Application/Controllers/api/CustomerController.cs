9a1b322d65e0fee4802bb52a10eaeb80278feccc
Sunku Saarthak <sunkusaarthak@gmail.com>
2024-04-16 20:03:43 +0530

    added various endpoints to customers controller

--- Movie Application/Controllers/api/CustomerController.cs
+++ Movie Application/Controllers/api/CustomerController.cs
@@ -1,9 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Movie_Application.Models;
+using System.Web.Http;
 
 namespace Movie_Application.Controllers.api
 {
-    [Route("api/[controller]")]
+    [System.Web.Http.Route("api/[controller]")]
     [ApiController]
     public class CustomerController : ControllerBase
     {
@@ -23,9 +24,53 @@ namespace Movie_Application.Controllers.api
             var customer = _context.Customers.FirstOrDefault(c => c.Id == id);
             if (customer == null)
             {
-                return new Customer { Name = "" };
+                throw new HttpResponseException(System.Net.HttpStatusCode.NotFound);
             }
             return customer;
+        }
+
+        [System.Web.Http.HttpPost]
+        public Customer PostCustomer(Customer customer)
+        {
+            if (!ModelState.IsValid)
+            {
+                throw new HttpResponseException(System.Net.HttpStatusCode.BadRequest);
+            }
+
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+
+            return customer;
+        }
+
+        public void UpdateCustomer(int Id, Customer customer)
+        {
+            if (!ModelState.IsValid)
+            {
+                throw new HttpResponseException(System.Net.HttpStatusCode.BadRequest);
+            }
+            var CurrentCustomer = _context.Customers.FirstOrDefault(c => c.Id == Id);
+            if (CurrentCustomer == null)
+            {
+                throw new HttpResponseException(System.Net.HttpStatusCode.BadRequest);
+            }
+            CurrentCustomer.Name = customer.Name;
+            CurrentCustomer.MembershipType = customer.MembershipType;
+            CurrentCustomer.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter;
+            CurrentCustomer.MembershipTypeId = customer.MembershipTypeId;
+            _context.SaveChanges();
+        }
+
+        [System.Web.Http.HttpDelete]
+        public void DeleteCustomer(int Id)
+        {
+            var CurrentCustomer = _context.Customers.FirstOrDefault(c => c.Id == Id);
+            if (CurrentCustomer == null)
+            {
+                throw new HttpResponseException(System.Net.HttpStatusCode.BadRequest);
+            }
+            _context.Customers.Remove(CurrentCustomer);
+            _context.SaveChanges();
         }
     }
 }
