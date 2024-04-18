e37b55124c9daeab9d8d25b2ef32c9a1a6ab4478
Sunku Saarthak <sunkusaarthak@gmail.com>
2024-04-18 23:16:22 +0530

    Added Movie Dto

---
+++ Movie Application/Dtos/MovieDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Movie_Application.Dtos
+{
+    public class MovieDto
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public int GenreId { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public DateTime DateInserted { get; set; }
+        public int Stock { get; set; }
+    }
+}
